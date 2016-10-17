5776a324da7aabff241be8d699a1b7b616642020
Johan <johans.inbox@gmail.com>
2016-10-17 19:25:36 +0200

    Added  warning

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const fs = require('fs');
 
 module.exports = {};
 
+/**
+ * Export the loader function
+ */
 module.exports = function(source, map) {
   const config = loaderUtils.parseQuery(this.query);
   const relativePath = '.' + this.resourcePath.slice(config.srcRoot.length);
@@ -16,7 +19,11 @@ module.exports = function(source, map) {
 
     fs.appendFileSync(config.collector, importStatement + exportStatement);
   }
+
   return source;
 };
 
+/**
+ * Export the plugin function
+ */
 module.exports.plugin = require('./plugin.js');
