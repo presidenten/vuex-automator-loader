0607ad00e813960b20c279495f993856f5487b8d
Johan Hernefeldt <johan.hernefeldt@teliasonera.com>
2016-10-17 12:35:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const loaderUtils = require("loader-utils");
+const path = require('path');
+const fs = require('fs');
+
+module.exports = {};
+
+module.exports.loader = function(source, map) {
+  const config = loaderUtils.parseQuery(this.query);
+  const relativePath = '.' + this.resourcePath.slice(config.srcRoot.length);
+  const moduleName = relativePath.slice(relativePath.lastIndexOf('/') + 1).slice(0, -3);
+  const file = fs.readFileSync(config.collector).toString();
+
+  if(file.indexOf(moduleName) < 0) {
+    const importStatement = 'import ' + moduleName + 'Module from \'' + relativePath + '\'; // eslint-disable-line\n';
+    const exportStatement = 'export const ' + moduleName + ' = ' + moduleName + 'Module;  // eslint-disable-line\n\n'
+
+    fs.appendFileSync(config.collector, importStatement + exportStatement);
+  }
+  return source;
+};
+
